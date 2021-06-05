b6ff1f2b22b51c4f9096019f4f1d38ff1f86301e
sudeshlak <swelikotuwa@gmail.com>
2021-06-06 05:15:30 +0530

    contat,about onlu route part completed

---
+++ src/Components/Contact.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Col, Row} from "react-bootstrap";
+
+const Contact: React.FC = () => {
+  return (
+      <Row className='contacts'>
+        <Col>
+          <h4 className='mt-2 mb-4'>Contact Us</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, cupiditate fuga minima molestiae quaerat quia sint? Adipisci aliquid debitis dolorem doloremque facilis iste maxime placeat temporibus? Corporis distinctio et impedit?</p>
+        </Col>
+      </Row>
+  );
+}
+
+export default Contact;
